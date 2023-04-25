6ade006d40147e2fe8f8e8ea92eb0cf3cdde7005
allium5566 <128320995+allium5566@users.noreply.github.com>
2023-04-25 23:19:03 +0800

    4/25

--- Homework/Frm_WindowSaver.Designer.cs
+++ Homework/Frm_WindowSaver.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_WindowSaver));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,6 +43,11 @@
             this.pictureBox1.Size = new System.Drawing.Size(156, 131);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Frm_WindowSaver
             // 
@@ -55,6 +62,7 @@
             this.Text = "Frm_WindowSaver";
             this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Frm_WindowSaver_MouseMove);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -63,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Homework/Frm_WindowSaver.cs
+++ Homework/Frm_WindowSaver.cs
@@ -13,14 +13,34 @@ namespace Homework
 {
     public partial class Frm_WindowSaver : Form
     {
+       
         
         public Frm_WindowSaver()
         {
             InitializeComponent();
+        
+        }private int xVelocity = 10;
+        private int yVelocity = 10;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 檢查圖片是否碰到了螢幕邊界，如果碰到了就反彈
+            if (pictureBox1.Location.X < 0 || pictureBox1.Location.X + pictureBox1.Width > ClientSize.Width)
+            {
+                xVelocity = -xVelocity;
+            }
+            if (pictureBox1.Location.Y < 0 || pictureBox1.Location.Y + pictureBox1.Height > ClientSize.Height)
+            {
+                yVelocity = -yVelocity;
+            }
 
-            
+            // 更新圖片位置
+            pictureBox1.Location = new Point(pictureBox1.Location.X + xVelocity, pictureBox1.Location.Y + yVelocity);
         }
         
 
+        private void Frm_WindowSaver_MouseMove(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
     }
 }
