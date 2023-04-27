ea955f4e163b2d2d721b6b125f05c33dd451f68c
allium5566 <j81103@gmail.com>
2023-04-27 17:25:15 +0800

    4/27

--- Homework/Frm_ForDoWhile.cs
+++ Homework/Frm_ForDoWhile.cs
@@ -129,16 +129,102 @@ namespace Homework
                 }
 
                 labResult.Text = $"{numFrom}到{numTo}相隔{numStep - 1} \n加總為 {sum.ToString()}";
-           
-            
-            
 
-            
+            }
 
+        private void btnwhile_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtFrom.Text, out int numFrom) || !int.TryParse(txtTo.Text, out int numTo) ||
+                !int.TryParse(txtStep.Text, out int numStep))
+            {
+                MessageBox.Show("請輸入數字");
+                return;
+            }
+
+            int sum =numFrom;
+            int i = 0;
             
-        
+            while (numFrom+(i++ *numStep)<numTo)
+            { 
+                sum += numFrom +( i * numStep);
+            }
+            labResult.Text = $"{numFrom}到{numTo}相隔{numStep - 1} \n加總為 {sum.ToString()}";
+        }
+
+        private void btndo_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtFrom.Text, out int numFrom) || !int.TryParse(txtTo.Text, out int numTo) ||
+              !int.TryParse(txtStep.Text, out int numStep))
+            {
+                MessageBox.Show("請輸入數字");
+                return;
+            }
+            int i = 0;
+            int sum =numFrom;
+            do
+            {
+                sum += numFrom + (i * numStep);
+            } while ((numFrom + i++) < numTo);
+        }
+
+        private void button6_Click(object sender, EventArgs e)//九九乘法表
+        {
+            string result = "";
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 2; j < 10; j++)
+                { 
+                    result += $" {j}×{i}＝{ (i*j).ToString("00")} | ";
+                }
+                result +=  "\n";
+            }
+              labResult.Text="九九乘法表\n"+ result ;
+        }
+
+        private void button5_Click(object sender, EventArgs e)//christmastree
+        {
+            string result = "";
+            if (!int.TryParse(txtRows.Text,out int rows))
+            {
+                MessageBox.Show("請輸入數字");
+                return;
+            }
+            for(int i = 1; i <= rows; i++)
+            {
+                for(int j=1;j<=i; j++)//*的數量(當i=3 ,j跑123
+                {
+                    result += "＊";
+                }
+                result += "\n";
+            }
+            labResult.Text=result;
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)//樂透開獎
+        {
+            int[] lottery = new int[49];
+           for(int i = 0; i <lottery.Length; i++)
+            {
+                lottery[i] = i+1;
+            }
+            int[] open = new int[6];
+            Random random = new Random();
+
+            for (int i = 0; i < open.Length; i++)
+            {
+                int index = random.Next(lottery.Length - i);
+                open[i] = lottery[index];
+                lottery[index] = lottery[lottery.Length - i - 1];
+            }
+            string result = "";
+            foreach(int i in open)
+            {
+                result += $"{i} ";
+                labResult.Text =$"樂透號碼\n [ {result} ]";
             }
         }
+    }
     }
 
        
